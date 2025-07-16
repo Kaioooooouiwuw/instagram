b1e476a21489e3e7529a7f71d9f39342bf120a6d
Guilherme Lima dos Santos Silva <oieeeo446@gmail.com>
2025-07-16 11:33:43 -0300

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;  // aqui
 
 // Middleware
 app.use(bodyParser.json());
